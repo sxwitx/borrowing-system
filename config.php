2a52586cbf1b9e25377caf8b62365e17a56aa20e
sxwitx <AomzHellowy@gmail.com>
2026-03-18 09:16:47 +0700

    Week 9-67704759 Borrow-System

--- config.php
+++ config.php
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
 <?php
-// การตั้งค่าสำหรับเชื่อมต่อฐานข้อมูลใน Docker
-$host = 'db'; // ใช้ 'db' ตามชื่อ service ของฐานข้อมูลใน docker-compose.yml
-$dbname = 'borrowings_db'; // ชื่อฐานข้อมูลใหม่ที่คุณกำหนด
+$host = 'db';
+$dbname = 'sports_borrow_db';
 $username = 'dev_user';
 $password = 'dev_password';
-
 try {
-    // สร้างการเชื่อมต่อฐานข้อมูลผ่าน PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
-    // หากเชื่อมต่อไม่สำเร็จ ให้แสดงข้อความแจ้งเตือน
     die("Connection failed: " . $e->getMessage());
 }
-=======
-<?php
-$host = 'localhost'; 
-$dbname = 'equipment_borrowing';
-$username = 'root';
-$password = '';
-
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
->>>>>>> ecb252f2b7e08ec439c774aa96e416c78811174b
 ?>
